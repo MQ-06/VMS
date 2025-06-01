1865a6b320cccda2627c06b71535dcb360d2bec0
Mariam Qadeem <mariamqadeem181@gmail.com>
2025-06-01 23:52:21 +0500

    error corrected

--- client/src/services/fleetService.js
+++ client/src/services/fleetService.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// For general usage (returns all fleets or filtered by query param)
 export const fetchFleets = (params = {}) => {
   const query = new URLSearchParams(params).toString();
   return axios.get(`/api/fleets${query ? `?${query}` : ''}`);
